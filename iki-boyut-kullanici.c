97cebcd4b4a72192426859d7b2aa50740a59f87e
Sevval Atalay <sevvalatalay7@gmail.com>
2023-04-20 00:02:55 +0300

    dizi

---
+++ iki-boyut-kullanici.c
@@ -0,0 +1,23 @@
+#include<stdio.h>
+main()
+{
+    int a[3][3];
+    int i,j;
+     for(i=0; i<3;i++)
+    {
+        for(j=0;j<3;j++)
+        {
+            printf("a[%d][%d] = ",i+1,j+1);
+            scanf("%d",&a[i][j]);
+        }
+       
+    }
+    for(i=0; i<3;i++)
+    {
+        for(j=0;j<3;j++)
+        {
+            printf("%d",a[i][j]);
+        }
+        printf("\n");
+    }
+}
