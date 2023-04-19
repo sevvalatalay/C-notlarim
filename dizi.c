51bb048a4095c2be2613ec147dbad91f479d20d5
Sevval Atalay <sevvalatalay7@gmail.com>
2023-04-19 17:49:41 +0300

    dizi tanimlama

---
+++ dizi.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+main() 
+{
+    int dizi[5];
+    int i;
+    for ( i = 0; i < 5; i++)
+    {
+        printf("dizi[%d] = ",i+1);
+        scanf("%d",&dizi[i]);
+        
+
+    }
+
+}
