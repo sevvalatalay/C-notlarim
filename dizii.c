6c0ab203080c4614eb040898e66b09dfb968462f
Sevval Atalay <sevvalatalay7@gmail.com>
2023-04-22 20:53:43 +0300

    c

---
+++ dizii.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+main()
+{
+    /* char a[100]="Antalya";
+    int i =0;
+    while (a[i]!='\0')
+{    printf("%c",a[i]);
+     i++;
+}  */
+// char a[100]="Antalya";
+    
+//     for (int i =0 ; a[i]!='\0'; i++)
+// {    printf("%c",a[i]);
+
+// }
+
+char a[10]="Van";
+printf("%s",a);
+
+
+}
